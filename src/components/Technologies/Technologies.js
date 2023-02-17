eab80929c01394ca3842ab288d9f1a6c3038d728
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2023-02-18 01:36:13 +0300

    Timeline complete

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,7 +4,7 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <Section id="#tech">
+  <Section id="tech">
     <SectionDivider />
     <br />
     <SectionTitle>Technologies</SectionTitle>
